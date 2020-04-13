86c329b1e824e5439931453d84dced195c71dfc6
dernathul <lautaroparracab@gmail.com>
2020-04-13 17:58:43 +0200

    fixing merging problems

--- src/components/EventsList.jsx
+++ src/components/EventsList.jsx
@@ -1,36 +1,32 @@
 import React, { Component } from "react";
 import axios from "axios";
-
-
 class EventsList extends Component {
   state = {
-    eventList: [],
-  }
+    eventsIndex: [],
+  };
 
   componentDidMount() {
-    axios.get("/events").then((response) => {
+    const axiosclient = axios.create({ baseURL: "http://localhost:3000" });
+    axiosclient.get("/events").then((response) => {
+      console.log(response);
       this.setState({
-        events: response.data.events,
-        
+        eventsIndex: response.data.events,
       });
+    });
+  }
 
-  render() 
-  debugger; { 
-    let showEvents
-    showEvents = (this.state.events.map(event) => {
-        return (
-          <div id = "event-title" key={event.id}>
-          <h1 >{event.title}</h1>
+  render() {
+    const { eventsIndex } = this.state;
+    const showEvents = eventsIndex.map((event) => {
+      return (
+        <div id={"event-" + event.id} key={event.id}>
+          <h3 id="title">{event.title}</h3>
+          <p id="description">{event.description}</p>
         </div>
-      )
-    }
-    )
-    return (
-      <div>
-   {showArticles}
-      </div>	
-    )	    
-  }	  
-
+      );
+    });
+    return <div>{showEvents}</div>;
+  }
 }
+
 export default EventsList;
