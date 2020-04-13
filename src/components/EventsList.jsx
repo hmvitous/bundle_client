674190b9fe7b722e1114d8b549c156d7fabcbd8a
Anish2504 <anish.kanswal@gmail.com>
2020-04-13 18:18:26 +0200

    listing events tests passing

--- src/components/EventsList.jsx
+++ src/components/EventsList.jsx
@@ -1,38 +1,29 @@
 import React, { Component } from "react";
 import axios from "axios";
-
 class EventsList extends Component {
   state = {
     eventsIndex: [],
   };
-
   componentDidMount() {
-    axios.get("/events").then(response => {
+    const axiosclient = axios.create({ baseURL: "http://localhost:3000" });
+    axiosclient.get("/events").then((response) => {
+      console.log(response);
       this.setState({
-        eventsIndex: response.data.events
+        eventsIndex: response.data.events,
       });
     });
   }
-
   render() {
-    const eventsIndex = this.state.eventsIndex;
-    let showEvents;
-    if (eventsIndex !== []) {
-      showEvents = eventsIndex.map(event => {
-        return (
-          
-          <div id={'event-' + event.id} key={event.id}>
+    const { eventsIndex } = this.state;
+    const showEvents = eventsIndex.map((event) => {
+      return (
+        <div id={"event-" + event.id} key={event.id}>
           <h3 id="title">{event.title}</h3>
           <p id="description">{event.description}</p>
         </div>
-          
-        );
-      });
-    }
-    return (
-      <div>{showEvents}</div>
-    );
+      );
+    });
+    return <div>{showEvents}</div>;
   }
 }
-
 export default EventsList;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
 
-axios.default.baseURL = "http://localhost3000/events";
+axios.default.baseURL = "http://localhost:3000";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
