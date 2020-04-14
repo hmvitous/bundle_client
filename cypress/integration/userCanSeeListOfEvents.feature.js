3e64f05ea24dc16a5ac938d46af027f623526af4
Anish2504 <anish.kanswal@gmail.com>
2020-04-14 12:12:39 +0200

    refactored the test file

--- cypress/integration/userCanSeeListOfEvents.feature.js
+++ cypress/integration/userCanSeeListOfEvents.feature.js
@@ -15,33 +15,19 @@ describe("user views the list of activities", () => {
 
     it("first event", () => {
       cy.get("#event-1").within(() => {
-        cy.get("#title").should("contain","Lets Play Soccer");
-          cy.get("#description").should(
-            "contain",
-            "We are a group of 20 people and we are missing 2 more to make two teams"
-          );
-        });
+        cy.get("#title").should("contain", "Lets Play Soccer");
+        cy.get("#description").should(
+          "contain",
+          "We are a group of 20 people and we are missing 2 more to make two teams"
+        );
+      });
 
-        describe("successfully displays", () => {
-          before(() => {
-            cy.server();
-            cy.route({
-              method: "GET",
-              url: "http://localhost:3000/events",
-              response: "fixture:events_list.json",
-            });
-          });
-
-          it("second event", () => {
-            cy.get("#event-2").within(() => {
-              cy.get("#title").should("contain","Walking on Sunshine");
-                cy.get("#description").should(
-                  "contain",
-                  "From Slussen to Skansen"
-                );
-              });
-            });
-          });
+      it("second event", () => {
+        cy.get("#event-2").within(() => {
+          cy.get("#title").should("contain", "Walking on Sunshine");
+          cy.get("#description").should("contain", "From Slussen to Skansen");
         });
       });
     });
+  });
+});
