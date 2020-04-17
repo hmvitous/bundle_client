92ae0f5d56787c602effbf11657cc7142456f93b
Anish2504 <anish.kanswal@gmail.com>
2020-04-17 08:56:42 +0200

    refactoring tests for seeing article

--- cypress/integration/userCanCreateEvent.feature.js
+++ cypress/integration/userCanCreateEvent.feature.js
@@ -1,6 +1,6 @@
 describe("user can create an event", () => {
   beforeEach(() => {
-    cy.visit("/")
+    cy.visit("/");
     cy.server();
     cy.route({
       method: "POST",
@@ -13,18 +13,14 @@ describe("user can create an event", () => {
     cy.get("#create-form").should("contain", "Title");
     cy.get("#create-form").should("contain", "Description");
     cy.get("#create-form").should("contain", "Category");
-    cy.get("#create-form").contains("Create Event").click();
   });
 
   it("Can create event", () => {
     cy.get("#create-form").within(() => {
       cy.get("#title").type("Play baseball");
       cy.get("#description").type("I need a lot of people");
-      // cy.get("#category").click()
-      // cy.get("#category").click("Sports")
       cy.get("button").contains("Submit").click();
     });
-    cy.get(".message").should("contain", "Your event has been created")
-
+    cy.get(".message").should("contain", "Your event has been created");
   });
 });
