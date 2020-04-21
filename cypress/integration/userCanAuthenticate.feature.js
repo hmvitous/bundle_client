dce1e53618ab8e490d267d88044ec3b13fb2f7ee
dernathul <lautaroparracab@gmail.com>
2020-04-21 10:24:00 +0200

    test for user login added

---
+++ cypress/integration/userCanAuthenticate.feature.js
@@ -0,0 +1,25 @@
+describe("User authenticates", () => {
+  beforeEach(() => {
+    crypto.visit("/");
+  });
+
+  it("successfully authenticate with valid credentials", () => {
+    cy.get("#login").click();
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("user@mail.com");
+      cy.get("#password").type("password");
+      cy.get("button").countains("Submit").click();
+    });
+    cy.get("#message").should("contain", "Hi user@mail.com");
+  });
+
+  it("unsuccessfully authenticate with invalid credentials", () => {
+    cy.get("#login").click();
+    cy.get("#login-form").within(() => {
+      cy.get("email").type("user@mail.com");
+      cy.get("#password").type("wronpassword");
+      cy.get("button").contains("Submit").click();
+    });
+    cy.get("#message").should("contain", "Invalid password");
+  });
+});
